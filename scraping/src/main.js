96dd2a175a2f931d27ec5676fb7debf96a5e9703
louisduhalberruer <pro@ldb.app>
2024-05-01 20:44:44 +0200

    feat: count des contributeurs

--- scraping/src/main.js
+++ scraping/src/main.js
@@ -1,48 +1,14 @@
 import 'dotenv/config'
-import {writeCSVFile} from "./utils/fs.js";
+import {addContributorsCount} from "./addContributorsCount.js";
+import {Octokit} from "octokit";
 
 const token = process.env.GITHUB_TOKEN;
-
-/*const octokit = new Octokit({
+const octokit = new Octokit({
   auth: token,
 });
 
-const resp = await octokit.request(
-  "GET /search/repositories?q=vue&sort=stars&order=desc",
-  {
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  },
-);
-
-for (const item of resp.data.items) {
-  await new Promise((resolve) => {
-    setTimeout(
-      async () => {
-        const contributors = await octokit.request(
-          `GET ${item.contributors_url}`,
-          {
-            headers: {
-              "X-GitHub-Api-Version": "2022-11-28",
-            },
-          },
-        );
-
-        item.contributors = contributors.data;
-        item.contributors_count = contributors.data.length;
-
-        resolve();
-      },
-      Math.floor(Math.random() * 1000),
-    );
-  });
-}
-*/
 function main() {
-  writeCSVFile('test')
+  addContributorsCount(octokit, 'githubStar1-10/data_1.csv', 'data_1.csv', 0, 10);
 }
-
 
 main();
-
--- scraping/src/utils/fs.js
+++ scraping/src/utils/fs.js
@@ -1,5 +1,5 @@
-import {stringify} from "csv/sync";
-import {writeFileSync, mkdirSync} from "fs";
+import {parse, stringify} from "csv/sync";
+import {mkdirSync, readFileSync, writeFileSync} from "fs";
 import {fileURLToPath, URL} from "node:url";
 
 export function writeCSVFile(name, content) {
@@ -8,20 +8,18 @@ export function writeCSVFile(name, content) {
   });
 
   const dirname = fileURLToPath(new URL(`../../dist/export`, import.meta.url))
-  const filename = fileURLToPath(new URL(`../../dist/export/${name}.csv`, import.meta.url))
+  const filename = fileURLToPath(new URL(`../../dist/export/${name}`, import.meta.url))
 
   mkdirSync(dirname, { recursive: true });
   writeFileSync(filename, out);
 }
 
 export function openCSVFile(path) {
-  const out = stringify(content, {
-    header: true,
-  });
+  const filename = fileURLToPath(new URL(`../../../${path}`, import.meta.url));
+  const input = readFileSync(filename, "utf-8");
 
-  const dirname = fileURLToPath(new URL(`../../dist/export`, import.meta.url))
-  const filename = fileURLToPath(new URL(`../../dist/export/${name}.csv`, import.meta.url))
-
-  mkdirSync(dirname, { recursive: true });
-  writeFileSync(filename, out);
+ return parse(input, {
+   columns: true,
+   skip_empty_lines: true
+ });
 }
